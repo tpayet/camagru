39e96fb9b8e4e1f6ee969e41e451f379e6d2a3cb
Thomas Payet <tpayet@student.42.fr>
2017-08-23 17:33:14 +0200

    Update user model with pwd handling

--- models/user.php
+++ models/user.php
@@ -4,5 +4,17 @@ require __DIR__."/model.php";
 
 class User extends Model
 {
+    public function set_pwd($password) {
+        $this->password = hash("sha512", "toto$password");
+    }
+
+    private function get_pwd():string {
+        return $this->password;
+    }
+
+    public static function login(PDO $dbh, string $username, string $password):bool {
+        $user = User::find($dbh, "username", $username);
+        return ($user->get_pwd() === hash("sha512", "toto$password"));
+    }
 }
 ?>
