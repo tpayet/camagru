92ec5f478cfd8649059dfa67693eac37fb3b526f
tpayet <tpayet@student.42.fr>
2017-08-27 18:55:28 +0200

    Add functionality for forgotten password WIP

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -8,8 +8,12 @@ require_once __DIR__."/../views/view.php";
 class LoginController extends Controller
 {
     public static function login(PDO $dbh, array $params) {
-        if (User::login($dbh, $params['username'], $params['password'])) {
+        if (array_key_exists("forgotten_pwd", $params)) {
+            $_SESSION["message"] = "an email to reset your password has been sent"
+        } elseif (User::login($dbh, $params['username'], $params['password'])) {
             $_SESSION['login'] = $params["username"];
+        } else {
+            $_SESSION["message"] = "login failed, either wrong credentials or account not confirmed";
         }
         echo View::render(__DIR__."/../views/index.php");
     }
