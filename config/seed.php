faab29ffb5f0705d30402f0ba49338bd7102d44d
tpayet <tpayet@student.42.fr>
2017-08-31 19:15:27 +0200

    Update seed with verbose

--- config/seed.php
+++ config/seed.php
@@ -5,8 +5,18 @@ require __DIR__."/../models/user.php";
 $dbh = require "database.php";
 
 User::create($dbh, array(
+                         "username" => "clecle",
+                         "email" => "clecle@gmail.com",
+                         "password" => "cle",
+                         "confirmed" => true));
+
+User::create($dbh, array(
                          "username" => "totolapaille",
                          "email" => "totolapaille@gmail.com",
                          "password" => "toto",
                          "confirmed" => true));
+
+print_r(User::where($dbh, "confirmed", false));
+
+print_r(User::find($dbh, "username", "totolapaille"));
 ?>
