e528414edcf85baf181973056fdf4aa628a36c3a
tpayet <tpayet@student.42.fr>
2017-09-01 14:48:45 +0200

    Update header template with get method for index access

--- templates/header.php
+++ templates/header.php
@@ -9,14 +9,14 @@
     </form>
     <?php if(array_key_exists("login", $_SESSION)) {?>
     <p> Welcome <?= $_SESSION["login"]?> </p>
-    <form action="/" method="post">
-        <input type="submit" name="index" value="/">
+    <form action="/">
+        <input type="submit" value="/">
     </form>
     <form action="/logout" method="post">
         <input type="submit" name="logout" value="logout">
     </form>
     <?php } else { ?>
-    <form action="/" method="post">
+    <form action="/">
         <input type="submit" name="login" value="login">
     </form>
     <form action="/sign_up" method="post">
