baa761bcd15319a3f4d20c5713bf0e8b67f30487
tpayet <tpayet@student.42.fr>
2017-09-09 14:50:15 +0200

    Update form validation with javascript and html

--- views/index.php
+++ views/index.php
@@ -6,7 +6,7 @@
 <body>
 <?php include __DIR__."/../templates/header.php" ?>
 <?php if (array_key_exists("login", $_SESSION)) { ?>
-    <div id="webcam">
+    <div id="webcam" style="background-color: blue;">
         <video id="video"></video>
         <canvas id="canvas" style="display: none"></canvas>
         <img id="photo" src="http://placekitten.com/g/320/261" alt="photo">
@@ -16,27 +16,27 @@
             <input type="submit" value="save picture">
         </form>
     </div>
-        <div id="preview">
-            <?php foreach($pictures as $img) { ?>
-                <div style="background-color: crimson; display: inline-block">
-                    <img src=<?= "/pictures?img_name=" . $img->get_name()?>>
-                </div>
-            <?php } ?>
-        </div>
+    <div id="last_pictures" style="background-color: orange;">
+        <?php foreach($pictures as $img) { ?>
+            <div style="background-color: crimson; display: inline-block">
+                <img src=<?= "/pictures?img_name=" . $img->get_name()?>>
+            </div>
+        <?php } ?>
+    </div>
     <div>
         <form enctype="multipart/form-data" method="POST" action="/upload">
             <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
-            <input type="file" name="userfile">
-            <input type="submit" value="upload file" />
+            <input type="file" name="userfile" accept="image/x-png,image/gif,image/jpeg">
+            <input type="submit" value="upload file"/>
         </form>
     </div>
 <?php } else { ?>
     <div>
         <form action="/login" method="post">
             <p>Login: </p>
-            <input type="text" name="username"><br>
+            <input type="text" name="username" required><br>
             <p>Pwd: </p>
-            <input type="password" name="password"><br>
+            <input type="password" name="password" required><br>
             <input type="submit" name="submit" value="login"><br>
             <input type="submit" name="forgotten_pwd" value="forgotten password">
         </form>
--- views/sign_up.php
+++ views/sign_up.php
@@ -7,9 +7,10 @@
     <?php include __DIR__."/../templates/header.php" ?>
     <div>
         <form action="/register" method="post">
-            <input type="text" name="username" placeholder="username"><br>
-            <input type="password" name="password" placeholder="password"><br>
-            <input type="email" name="email" placeholder="email"><br>
+            <p>password must contain at least one special char, one uppercase letter, one lowercase letter, one digit and a minimum length of 6 characters</p><br>
+            <input type="text" name="username" placeholder="username" required><br>
+            <input type="password" name="password" placeholder="password" required pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$"><br>
+            <input type="email" name="email" placeholder="email" required><br>
             <input type="submit" name="register">
         </form>
     </div>
