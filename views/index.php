445a8c259e980e766e03007c0e6f804b1da9b7f5
tpayet <tpayet@student.42.fr>
2017-09-15 16:58:15 +0200

    Wip js filter on index

--- views/index.php
+++ views/index.php
@@ -1,26 +1,38 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Camagru</title>
-</head>
-<body>
-<?php include __DIR__."/../templates/header.php" ?>
-<?php if (array_key_exists("login", $_SESSION)) { ?>
-    <div id="webcam" style="background-color: blue;">
+    <head>
+        <title>Camagru</title>
+    </head>
+    <style type="text/css">
+    .img_filter {
+    max-height: 100px;
+    max-width: 100px;
+    }
+    </style>
+    <body>
+        <?php include __DIR__."/../templates/header.php" ?>
+        <?php if (array_key_exists("login", $_SESSION)) { ?>
+        <div id="webcam" style="background-color: blue;">
         <video id="video"></video>
         <canvas id="canvas" style="display: none"></canvas>
         <img id="photo" src="http://placekitten.com/g/320/261" alt="photo">
         <button id="startbutton">Prendre une photo</button>
         <form action="/save_webcam" method="post">
             <input id="photo_input" type="hidden" name="data">
+            <input type="radio" name="filter" value="old_school" onclick="">
+            <label><img class="img_filter" src="https://i.pinimg.com/originals/f0/a5/0b/f0a50ba3e0614fa458003d67bbb4f395.jpg"></label>
+            <input type="radio" name="filter" value="flower" required>
+            <label><img class="img_filter" src="https://i.pinimg.com/originals/4f/93/8d/4f938d6a0398679ecac42be7dc327788.png"></label>
             <input type="submit" value="save picture">
         </form>
     </div>
+    <div>
+    </div>
     <div id="last_pictures" style="background-color: orange;">
         <?php foreach($pictures as $img) { ?>
-            <div style="background-color: crimson; display: inline-block">
-                <img src=<?= "/pictures?img_name=" . $img->get_name()?>>
-            </div>
+        <div style="background-color: crimson; display: inline-block">
+            <img src=<?= "/pictures?img_name=" . $img->get_name()?>>
+        </div>
         <?php } ?>
     </div>
     <div>
@@ -30,7 +42,7 @@
             <input type="submit" value="upload file"/>
         </form>
     </div>
-<?php } else { ?>
+    <?php } else { ?>
     <div>
         <form action="/login" method="post">
             <p>Login: </p>
@@ -41,8 +53,8 @@
             <input type="submit" name="forgotten_pwd" value="forgotten password">
         </form>
     </div>
-<?php } ?>
-<?php  include __DIR__."/../templates/footer.php" ?>
+    <?php } ?>
+    <?php  include __DIR__."/../templates/footer.php" ?>
 </body>
 <?php if (array_key_exists("login", $_SESSION)) { ?>
 <script type="text/javascript" src=<?= "/serve_js?script=webcam.js" ?>></script>
